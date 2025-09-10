99f2c710116ae01205b2448d8c84513401ea648c
Ilyes HAMDI <ilyesilyeshamdi10@gmail.com>
2025-09-10 17:41:48 +0100

    update

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from .api.routes import auth,user,article,appointment,notifications
+from fastapi.responses import RedirectResponse
 
 app=FastAPI()
+@app.get("/", include_in_schema=False)
+def redirect_to_docs():
+    return RedirectResponse(url="/docs")
 
 app.include_router(user.router)
 app.include_router(auth.router)
